2a745a1734300701119218db8984eb95ec88cbba
= <shawaarma0200@gmail.com>
2022-12-20 18:57:59 +0300

    nodemailer

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
-if (process.env.NODE_ENV === 'production') {
+//if (process.env.NODE_ENV === 'production') {
   //*Set static folder
-  app.use(express.static('client/build'));
+  //app.use(express.static('client/build'));
   
-  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-}
+  //app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+//}
 
 
 //error handler
